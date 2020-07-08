6214a1899cd23653d20a3af967b6df8caa0e559b
Nichole C Cobb <nichole.c.cobb@gmail.com>
2020-07-08 03:33:30 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,10 @@
-# Define the variable board below.
-row = [" ", " "," "," "," "," "," "," "," "]
+row = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+row[0]
+row[1]
+row[2]
+row[3]
+row[4]
+row[5]
+row[6]
+row[7]
+row[8]
